2e671c4d476512a12ea2fc04a3e9dbd802a85f54
Mikael Yeghiazaryan <mikaegiazaryan2000@gmail.com>
2022-08-29 19:40:56 +0300

    Added dataloaders

--- train.py
+++ train.py
@@ -1,12 +1,28 @@
 import torch
 import os, glob
 from dataset import MRIDataset
+from torch.utils.data import DataLoader
 
 import config
 
-"""Load the data set"""
-train_img_path = os.path.join(config.ROOT_PATH, "data/train/image/")
-train_mask_path = os.path.join(config.ROOT_PATH, "data/train/mask/")
+"""CONFIG"""
+ROOT_PATH = config.ROOT_PATH
+batch_size = 16
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+"""Load the data set"""
+train_img_path = os.path.join(ROOT_PATH, "data/train/image/")
+train_mask_path = os.path.join(ROOT_PATH, "data/train/mask/")
+val_img_path = os.path.join(ROOT_PATH, "data/val/image/")
+val_mask_path = os.path.join(ROOT_PATH, "data/val/mask/")
+
+# Create the Dataset classes for the train and validation data sets
 train_data = MRIDataset(train_img_path, train_mask_path)
-image, mask = train_data[1]
+val_data = MRIDataset(val_img_path, val_mask_path)
+
+"""Create the dataloaders"""
+train_loader = DataLoader(train_data, batch_size=batch_size)
+val_loader = DataLoader(val_data, batch_size=batch_size)
+
+"""Initialize the model"""
+pass
