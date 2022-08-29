7fd7432faa4dfa103ed5b123600115f3a0f4b7a3
Mikael Yeghiazaryan <mikaegiazaryan2000@gmail.com>
2022-08-29 19:34:13 +0300

    Added a custom data set class

--- dataset.py
+++ dataset.py
@@ -1,9 +1,41 @@
 from torch.utils.data import Dataset
+import glob
+import cv2
+import pdb
 
 class MRIDataset(Dataset):
     def __init__(self, images_path, masks_path) -> None:
+        # Save the images and masks paths
         self.images_path = images_path
         self.masks_path = masks_path
 
-    def extract_files(self, files_path):
-        pass
+        # Extract the full paths of all images and their corresponding masks
+        self.images = self.extract_filepaths(images_path)
+        self.masks = self.extract_filepaths(masks_path)
+
+    def extract_filepaths(self, files_path):
+        files = list()
+        for file in glob.glob(files_path + "/*.tif"):
+            files.append(file)
+        return files
+
+    def __getitem__(self, index):
+        def load_image(image_path, grayscale):
+            if grayscale:
+                img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+            else:
+                img = cv2.imread(image_path)
+                img = img[...,::-1] # the image is loaded as a BGR -- convert it to RGB
+            return img
+        
+        image_path = self.images[index]
+        mask_path = self.masks[index]
+
+        image = load_image(image_path, grayscale=False)
+        mask = load_image(mask_path, grayscale=True)
+
+        return (image, mask)
+    
+    def __len__(self):
+        assert len(self.images) == len(self.masks)
+        return len(self.images)
--- train.py
+++ train.py
@@ -1,7 +1,12 @@
 import torch
 import os, glob
+from dataset import MRIDataset
 
 import config
 
 """Load the data set"""
-data_path = os.path.join(config.ROOT_PATH, "data/")
+train_img_path = os.path.join(config.ROOT_PATH, "data/train/image/")
+train_mask_path = os.path.join(config.ROOT_PATH, "data/train/mask/")
+
+train_data = MRIDataset(train_img_path, train_mask_path)
+image, mask = train_data[1]
